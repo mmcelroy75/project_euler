05673feb40c125dac66c12b8fcb1b61ebea12cb5
Mark McElroy <Mark@Excerceo-Novus.local>
2020-04-18 15:37:18 -0700

    completed project 1

--- project1.py
+++ project1.py
@@ -1,6 +1,11 @@
 multiples_lst = []
 
-multiples_lst.append(4)
-multiples_lst.append(5)
+i = 0
+
+for i in range(1000):
+    if (i % 3 == 0) or (i % 5 == 0):
+        multiples_lst.append(i) 
 
 print(sum(multiples_lst[:]))
+
+#sum is equal to 23,3168
